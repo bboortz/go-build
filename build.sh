6cf70586a2ef5dd55382dc385169f591902f94a9
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2017-08-13 01:28:42 +0200

    more readable log output during first build /BB

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
 set -o nounset
@@ -7,16 +7,21 @@ set -o pipefail
 GOBUILD=$( which go-build 2> /dev/null || echo -n )
 if [ -z "$GOBUILD" ]; then
 	if [ -f ./go-build ]; then
+		echo
 		echo "INFO: build with local application: ./go-build"
 		GOBUILD=./go-build
 	else
+		echo
 		echo "INFO: build build-container: go-build-base"
 		cd dockerimage
 		docker build -t go-build-base -f Dockerfile .
 		cd ..
+		echo
 		echo "INFO: build application: go-build"
 		docker run -it -u $(id -u):$(id -g) -v $(pwd):/go/src/github.com/bboortz/go-build -w /go/src/github.com/bboortz/go-build -e APP=go-build -e ARCH=amd64 go-build-base /build/build.sh
 		GOBUILD=./go-build
+		echo
+		echo "INFO: build application again with go-build"
 	fi
 fi
 
