1b0e260dd2ea4487db36f3010ab35fd8e8d52b4a
Benjamin Boortz <benjamin.boortz@volkswagen.de>
2017-08-13 00:29:06 +0200

    build without go-build added /BB

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-GOBUILD=$( which go-build 2> /dev/null )
+GOBUILD=$( which go-build 2> /dev/null || echo -n )
 if [ -z "$GOBUILD" ]; then
 	if [ -f ./go-build ]; then
 		echo "INFO: build with local application: ./go-build"
 		GOBUILD=./go-build
 	else
+		echo "INFO: build build-container: go-build-base"
+		cd dockerimage
+		docker build -t go-build-base -f Dockerfile .
+		cd ..
 		echo "INFO: build application: go-build"
 		docker run -it -u $(id -u):$(id -g) -v $(pwd):/go/src/github.com/bboortz/go-build -w /go/src/github.com/bboortz/go-build -e APP=go-build -e ARCH=amd64 go-build-base /build/build.sh
 		GOBUILD=./go-build
